99b7f7e95b9f8b39a7b453c3d806c2661f93654c
pitohui02 <stimpode45@gmail.com>
2023-12-12 09:25:08 +0800

    update: landing page

--- components/navbar.php
+++ components/navbar.php
@@ -11,12 +11,12 @@
             <nav class = "nav">
                 <img src="public/renton-logo.png" width="150" height="150">
                 <div class = "nav_link">
-                    <a href = "home">Home </a>
+                    <a href = "landing.php">Home </a>
                     <a href = "explore">Explore </a>
                     <a href = "ownercollab">Owner Collaborations </a>
                     <a href = "contact">Contact Us </a>
                     <a href = "about">About Us </a>
-                    <a href = "login">Login / Signup </a>
+                    <a href = "login.php">Login / Signup </a>
                 </div>
 
             </nav>
